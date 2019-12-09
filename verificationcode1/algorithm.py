80de583a396135d140a06d87bacd89130b9c23c3
517839MY@pw <2965372064@qq.com>
2019-12-09 16:45:47 +0800

    verification code type one

---
+++ verificationcode1/algorithm.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import pytesseract
+
+def reduct_noise(path):
+    #open the image
+    image = Image.open(path)
+    
+    #convert it to grayscale 'L'
+    gray_img = image.convert(mode='L')
+
+    #binarization
+    threshold = 94.35
+    table = []
+    for i in range(256):
+        if i < threshold:
+            table.append(0)
+        else:
+            table.append(1)
+    #map the look up table into the image
+    out_img = gray_img.point(lut=table,mode='1')
+
+    custom_oem_psm_config = r'--psm 7'
+
+    #pull the text out of image
+    code = pytesseract.image_to_string(out_img, config=custom_oem_psm_config)
+
+    return code
---
+++ verificationcode1/main.py
@@ -0,0 +1,5 @@
+import algorithm
+
+if __name__ == "__main__":
+    code = algorithm.reduct_noise("img.jpg")
+    print(code)
